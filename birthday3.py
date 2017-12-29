5df964496f8eb4e5d7b0482d3859574d57525db0
JerryCui <jianwei824@sina.com>
2017-12-29 16:11:09 +0800

    just print string

--- birthday3.py
+++ birthday3.py
@@ -1,10 +1,11 @@
 '''Function definition and invocation.'''
 
-def happyBirthdayEmily():
+def happy_birthday_emily():
+    """just print string"""
     print("Happy Birthday to you!")
     print("Happy Birthday to you!")
     print("Happy Birthday, dear Emily.")
     print("Happy Birthday to you!")
 
-happyBirthdayEmily()
-happyBirthdayEmily()
+happy_birthday_emily()
+happy_birthday_emily()
