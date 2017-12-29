c25d56f561c0e8ff3b5e29cff9acf443301d74c7
JerryCui <jianwei824@sina.com>
2017-12-29 15:52:01 +0800

    双星号 可变参数 locals

--- arithDict.py
+++ arithDict.py
@@ -1,9 +1,11 @@
 '''Fancier format string example, with locals().'''
 
-x = 20
-y = 30
-sum = x + y
-prod = x * y
-formatStr = '{x} + {y} = {sum}; {x} * {y} = {prod}.'
-equations = formatStr.format(**locals())
-print(equations)
+def foo(x,y):
+    sum = x + y
+    prod = x * y
+    formatStr = '{x} + {y} = {sum}; {x} * {y} = {prod}.'
+    equations = formatStr.format(**locals())
+    print(equations)
+
+if __name__ == '__main__':
+    foo(20,30)
