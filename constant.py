153924d7ee18ebd2b56bb6191cd5e461671477dd
JerryCui <jianwei824@sina.com>
2017-12-29 16:47:09 +0800

    while method

--- constant.py
+++ constant.py
@@ -2,14 +2,17 @@
 
 PI = 3.14159265358979   # global constant -- only place the value of PI is set
 
-def circleArea(radius):
+def circle_area(radius):
+    """circle area caculate"""
     return PI*radius*radius    # use value of global constant PI
 
-def circleCircumference(radius):
+def circle_circumference(radius):
+    """circumference caculate"""
     return 2*PI*radius         # use value of global constant PI
 
 def main():
-    print('circle area with radius 5:', circleArea(5))
-    print('circumference with radius 5:', circleCircumference(5))
+    """constant"""
+    print('circle area with radius 5:', circle_area(5))
+    print('circumference with radius 5:', circle_circumference(5))
 
 main()
--- cool.py
+++ cool.py
@@ -8,5 +8,7 @@ while TEMPERATURE > 90: # first while loop code
     print(TEMPERATURE)
     TEMPERATURE = TEMPERATURE - 1
     sleep(DELAY_TIME) # be cool too fast to like reality
+else:
+    print("Now")
 
 print('The tea is cool enough.')
