f4b49f487ba7dcabc648f8ca61911e29e54bf964
jerry <jianwei824@sina.com>
2018-01-03 23:34:37 +0800

    make wrong right

--- numberEntriesWRONG.py
+++ numberEntriesWRONG.py
@@ -1,7 +1,8 @@
 def numberList(items): #WRONG code for illustration
     '''Print each item in a list items, numbered in order.'''
+    number = 1
     for item in items:
-        number = 1
+        #number = 1
         print(number, item)
         number = number + 1
 
