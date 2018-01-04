dc1d8ab4ed052630a6c9ee092016cdbcb6b4913f
JerryCui <jianwei824@sina.com>
2018-01-04 11:57:27 +0800

    number

--- numberEntries3.py
+++ numberEntries3.py
@@ -2,4 +2,4 @@ items = ['red', 'orange', 'yellow', 'green']
 number = 1                                   
 for item in items:   # print numbered entries
     print(number, item)                      
-    number = number + 1   # crucial added line
+    number += 1   # crucial added line
