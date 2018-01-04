613c21d453f1ab4a98896d7f2e7c55a7b6212309
JerryCui <jianwei824@sina.com>
2018-01-04 12:42:09 +0800

    file operation

--- revisedFile.py
+++ revisedFile.py
@@ -2,3 +2,9 @@ outFile = open('sample3.txt', 'w')
 outFile.write('A revised output file!\n')
 outFile.write('Write some more.\n')
 outFile.close()
+
+"""read from file and print"""
+readfile = open("sample3.txt", "r")
+content = readfile.read()
+readfile.close()
+print(content)
